3eacf91d108c2ad6e7cf8004967eb258bae9ca6f
Aman Sharma <amsr@google.com>
2025-09-11 13:31:29 -0400

    docker image for ctl

--- cmd/demo-run/main.go
+++ cmd/demo-run/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
 
 	"github.com/go-git/go-billy/v5/osfs"
-	"github.com/google/oss-rebuild/internal/api/inferenceservice"
 	"github.com/google/oss-rebuild/pkg/build"
 	"github.com/google/oss-rebuild/pkg/build/local"
 	"github.com/google/oss-rebuild/pkg/rebuild/rebuild"
@@ -31,43 +31,40 @@ func runBuild(coordinates []string, out chan<- string) {
 
 	ctx := context.Background()
 
-	schemaStrategy, inferErr := inferenceservice.Infer(ctx, schema.InferenceRequest{
-		Ecosystem: rebuild.Maven,
-		Package:   pkg,
-		Version:   version,
-		Artifact:  artifact,
-	}, &inferenceservice.InferDeps{
-		HTTPClient: http.DefaultClient,
-		GitCache:   nil,
-	})
+	inferenceOutputBufferStdout := &bytes.Buffer{}
+	inferenceOutputBufferStderr := &bytes.Buffer{}
 
 	directory := path.Join(*localStore, "inference-logs", pkg, version, artifact)
 	os.MkdirAll(directory, 0755)
-	if inferErr != nil {
-		inferenceFailureLog, err := os.Create(path.Join(directory, "logs"))
-		if err != nil {
-			log.Fatal(errors.Wrap(err, "creating inference failure log"))
-		}
-		defer inferenceFailureLog.Close()
-		inferenceFailureLog.WriteString(fmt.Sprintf("%v\n", inferErr))
-		out <- fmt.Sprintf("Inference failure for %s:%s:%s\n", pkg, version, artifact)
+	inferenceLog, err := os.Create(path.Join(directory, "logs"))
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "creating inference log"))
+	}
+	defer inferenceLog.Close()
+
+	cmd := exec.CommandContext(ctx, "docker", "run", "--rm", "--memory", "10g", "ctl", "infer",
+		"--ecosystem", "maven",
+		"--package", pkg,
+		"--version", version,
+		"--artifact", artifact)
+	cmd.Stdout = inferenceOutputBufferStdout
+	cmd.Stderr = inferenceOutputBufferStderr
+
+	err = cmd.Run()
+
+	inferenceLog.WriteString(fmt.Sprintf("%sSTDOUT:\n%s\n\nSTDERR:\n%s\n%v\n", cmd.String(), inferenceOutputBufferStdout.String(), inferenceOutputBufferStderr.String(), err))
+
+	if err != nil {
+		out <- fmt.Sprintf("Inference failure for %s:%s:%s: %v\n", pkg, version, artifact, err)
 		return
 	}
+
+	var schemaStrategy schema.StrategyOneOf
+	json.NewDecoder(inferenceOutputBufferStdout).Decode(&schemaStrategy)
 
 	s, err := schemaStrategy.Strategy()
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "parsing strategy"))
-	}
-
-	inferenceSuccessLog, err := os.Create(path.Join(directory, "logs"))
-	if err != nil {
-		log.Fatal(errors.Wrap(err, "creating inference success log"))
-	}
-	defer inferenceSuccessLog.Close()
-	enc := json.NewEncoder(inferenceSuccessLog)
-	enc.SetIndent("", "  ")
-	if err := enc.Encode(schemaStrategy); err != nil {
-		log.Fatal(errors.Wrap(err, "encoding result"))
 	}
 
 	inp := rebuild.Input{Target: rebuild.Target{
